003c98b6c8d6472016271c9a6780d4feac0292fa
deano1949 <gly1949@gmail.com>
2023-05-27 21:26:29 +0100

    Created

---
+++ Development/Master_tuning_TLT.m
@@ -0,0 +1,105 @@
+% Setting
+clc;clear;
+
+loaddata='N';
+TradeOptionType_range={'1stM','2ndM'};%1stM
+OptionMoneyness_range={'95OTM','97OTM','ATM','103ITM','105ITM'};%Trade 90OTM 95OTM ATM 105ITM 110ITM
+dynamic_weight='Y'; %'Decsend_only'; 'Y'
+dynamic_weight_type_range={'FixedLeverage1',...
+    'FixedLeverage1.5','FixedLeverage1.8','FixedLeverage2.5',...
+    'YueDieYueSell210','YueDieYueSell1.510','YueDieYueSell21.50','YueDieYueSell2.51.80','YueDieYueSell320'};
+
+%% Output setting
+Todaystamp=datestr(today,'YYYY_mm_DD');
+OutputExcel=['Tuning_output\Master_tuning_output_TLT' Todaystamp '.xlsx'];
+%% Get data & Setting
+if strcmp(loaddata,'Y')
+    addpath('C:\Users\gly19\Dropbox\GU\1.Investment\4. Alphas (new)\42. Sigmaa003_Enhanced\Data');
+    loadopitondata();
+end
+
+load('C:\Users\gly19\Dropbox\GU\1.Investment\4. Alphas (new)\42. Sigmaa003_Enhanced\Data\TLTOptionData.mat');
+
+
+%% Load BMK
+OutputCollection_BMK = BMK(tlt_price,Datestamp_tlt,'TLT');
+OutputCollection_TY = BMK(ty_prices,Datestamp_tlt,'TY10');
+
+Master_tuning_output=struct;
+CollectionTable=OutputCollection_BMK.PerformanceStats;
+CollectionCumpnl=OutputCollection_BMK.PerformanceTS.Cumpnl;
+
+CollectionTable=vertcat(CollectionTable,OutputCollection_TY.PerformanceStats);
+CollectionCumpnl=horzcat(CollectionCumpnl,OutputCollection_TY.PerformanceTS.Cumpnl);
+CollectionTable.Properties.RowNames={'BMK','TY10'};
+
+%% Loops start here
+for i=1:size(TradeOptionType_range,2)
+    TradeOptionType=TradeOptionType_range{i};
+
+    % Trade 1st or 2nd Month Expiring options
+    if strcmp(TradeOptionType,'1stM')
+        Ivolcurve=Ivolcurve_1stM_tlt;
+    elseif strcmp(TradeOptionType,'2ndM')
+        Ivolcurve=Ivolcurve_2ndM_tlt;
+    end
+
+    for j=1:size(OptionMoneyness_range,2)
+        OptionMoneyness=OptionMoneyness_range{j};
+
+        for k=1:size(dynamic_weight_type_range,2)
+            dynamic_weight_type=dynamic_weight_type_range{k};
+
+            % Setting for trade simulation can be found in sigmaa003_2023.m
+           % Below is the default numbers
+            Initial_AUM=200000;
+            Trading_cost_per_contract=0.3; %trading cost per contract
+            Leverage_lever=1; %Overall leverage control
+
+            % Dyanmics weighting options
+            switch dynamic_weight_type
+                case 'YueDieYueBuy112' %(YDYBuy越跌越买
+                    Vix_Thld=[13 20 50]; Leverge_step=[1 1 2];
+                case 'YueDieYueBuy111.5' %
+                    Vix_Thld=[13 20 50]; Leverge_step=[1 1.5 2];
+                case 'FixedLeverage1' %
+                    Vix_Thld=[13 20 50]; Leverge_step=[1 1 1];
+                case 'FixedLeverage1.5' %
+                    Vix_Thld=[13 20 50]; Leverge_step=[1.5 1.5 1.5];
+                case 'FixedLeverage1.8' %
+                    Vix_Thld=[13 20 50]; Leverge_step=[1.8 1.8 1.8];
+                case 'FixedLeverage2.5' %
+                    Vix_Thld=[13 20 50]; Leverge_step=[2.5 2.5 2.5];
+                case 'YueDieYueSell210' %
+                    Vix_Thld=[13 20 50]; Leverge_step=[2 1 1];
+                case 'YueDieYueSell1.510' %
+                    Vix_Thld=[13 20 50]; Leverge_step=[1.5 1 1];
+                case 'YueDieYueSell21.50' %
+                    Vix_Thld=[13 20 50]; Leverge_step=[2 1.5 1.5];
+                case 'YueDieYueSell2.51.80' %
+                    Vix_Thld=[13 20 50]; Leverge_step=[2.5 1.8 1.8];
+                case 'YueDieYueSell320' %
+                    Vix_Thld=[13 20 50]; Leverge_step=[3 2 2];
+                otherwise
+                    error('choose dynamic_weight_type');
+            end
+
+
+            % Main line
+            [OutputCollection] = sigmaa003_2023(tlt_price,Ivolcurve_1stM_tlt,Ivolcurve_2ndM_tlt,Vix_dat_tlt,ExpiryDate_tlt,Datestamp_tlt,TradeOptionType,OptionMoneyness,dynamic_weight,Vix_Thld,Leverge_step);
+            loop_name=[ 'L_' TradeOptionType '_' OptionMoneyness '_' dynamic_weight_type];
+            loop_name=strrep(loop_name,'.','_');
+            Master_tuning_output.(loop_name)=OutputCollection;
+
+            save(['Tuning_output\Master_tuning_output_TLT_' Todaystamp '.mat'],"Master_tuning_output","-mat");
+
+            CollectionRowName=vertcat(CollectionTable.Properties.RowNames,loop_name);
+            CollectionTable=vertcat(CollectionTable,OutputCollection.PerformanceStats);
+            CollectionCumpnl=horzcat(CollectionCumpnl,OutputCollection.PerformanceTS.Cumpnl);
+            CollectionTable.Properties.RowNames=CollectionRowName;
+        end
+    end
+end
+CollectionCumpnl=array2timetable(CollectionCumpnl,"RowTimes",Datestamp_tlt,'VariableNames',CollectionRowName);
+writetable(CollectionTable,OutputExcel,'WriteRowNames',true,'Sheet','StatsTable','UseExcel',1);
+writetimetable(CollectionCumpnl,OutputExcel,'Sheet','CumPNLTS','UseExcel',1);
---
+++ Production/BMK_prod.m
@@ -0,0 +1,38 @@
+function [OutputCollection] = BMK_prod(data_ts,Datestamp,BMKname)
+BMK_PI=data_ts(:,1);
+Rf=data_ts(:,3);
+
+
+Dailyreturn=[0; tick2ret(BMK_PI)];
+Excessreturn=Dailyreturn-Rf/250/100;
+Cumpnl=cumprod(1+Dailyreturn)*100;
+
+%Since inception (2005)
+cumpnl_SI=prod(1+Dailyreturn)-1; %Accumulative PNL
+apr_SI=prod(1+Dailyreturn).^(252/length(Dailyreturn))-1; %annualised returns since inception
+sharperatio_SI=mean(Excessreturn)*sqrt(252)/std(Excessreturn); %sharpe ratio since inception
+Volatility=std(Dailyreturn)*sqrt(252);
+maxdd_SI=maxdrawdown(Cumpnl);
+% 1Y
+cumpnl_1Y=prod(1+Dailyreturn(end-252:end))-1; 
+apr_1Y=prod(1+Dailyreturn(end-252:end)).^(252/length(Dailyreturn(end-252:end)))-1; 
+sharperatio_1Y=mean(Excessreturn(end-252:end))*sqrt(252)/std(Excessreturn(end-252:end));
+% 3Y
+cumpnl_3Y=prod(1+Dailyreturn(end-252*3:end))-1; 
+apr_3Y=prod(1+Dailyreturn(end-252*3:end)).^(252/length(Dailyreturn(end-252*3:end)))-1; 
+sharperatio_3Y=mean(Excessreturn(end-252*3:end))*sqrt(252)/std(Excessreturn(end-252*3:end)); 
+% 5Y
+cumpnl_5Y=prod(1+Dailyreturn(end-252*5:end))-1; 
+apr_5Y=prod(1+Dailyreturn(end-252*5:end)).^(252/length(Dailyreturn(end-252*5:end)))-1; 
+sharperatio_5Y=mean(Excessreturn(end-252*5:end))*sqrt(252)/std(Excessreturn(end-252*5:end)); 
+
+
+PerformanTbl=array2table([cumpnl_SI apr_SI sharperatio_SI Volatility maxdd_SI cumpnl_1Y apr_1Y sharperatio_1Y ...
+    cumpnl_3Y apr_3Y sharperatio_3Y cumpnl_5Y apr_5Y sharperatio_5Y],'VariableNames',{'Total_return_SI','APR_SI','SharpeRatio_SI','Volatility','MaxDrawdown_SI',...
+    'Total_return_1Y','APR_1Y','SharpeRatio_1Y','Total_return_3Y','APR_3Y','SharpeRatio_3Y','Total_return_5Y','APR_5Y','SharpeRatio_5Y'});
+
+OutputCollection=struct;
+OutputCollection.Setting={'BMK',BMKname,'Long_only','No_Leverage'};
+OutputCollection.PerformanceStats=PerformanTbl;
+OutputCollection.PerformanceTS=array2timetable([Dailyreturn Excessreturn Cumpnl],'RowTimes',Datestamp,'VariableNames',{'Dailyreturn','Excessreturn','Cumpnl'});
+
